c2f5991c0fa310a88775d3b581dd27157a1e85f2
patthyyy <153175681+patthyyy@users.noreply.github.com>
2023-12-08 18:19:26 +0700

    hist5

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 # import openai
 import json
 import pandas as pd
 
 user_api_key = st.sidebar.text_input("OpenAI API key", type= "password")
 
-client = openai.OpenAI(api_key = user_api_key)
+client = OpenAI(api_key = user_api_key)
 
 st.title('Writing tutor')
 st.markdown('Input the writing that you want to improve.\n\ The AI will give you suggestions on how to improve it.')
