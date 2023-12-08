c078fb14967bb82d40810148f9b24563fd3e6c59
patthyyy <153175681+patthyyy@users.noreply.github.com>
2023-12-08 18:10:15 +0700

    hist3

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
 import streamlit as st
-from langchain.llms import OpenAI
+import openai
+import json
+import pandas as pd
 
-st.title('🦜🔗 Quickstart App')
+user_api_key = st.sidebar.text_input("OpenAI API key", type= "password")
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+client = openai.OpenAI(api_key = user_api_key)
 
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+st.title('Writing tutor')
+st.markdown('Input the writing that you want to improve.\n\ The AI will give you suggestions on how to improve it.')
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+user_input = st.text_area("Enter some text to correct:", "your text heare")
