6b33aaebb8c36d79ef681dbf144fbb492965d111
patthyyy <153175681+patthyyy@users.noreply.github.com>
2023-12-10 15:34:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,4 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type= "password")
 
 client = OpenAI(api_key = user_api_key)
 
-st.title('Writing tutor')
-st.markdown('Input the writing that you want to improve.\n\ The AI will give you suggestions on how to improve it.')
-
 user_input = st.text_area("Enter some text to correct:", "your text heare")
