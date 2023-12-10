e4d06dedb8883e747bb4cbdc9928451e0bb4b0db
patthyyy <153175681+patthyyy@users.noreply.github.com>
2023-12-10 15:42:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ import openai
 import json
 import pandas as pd
 
+st.title('Quickstart App')
+
 user_api_key = st.sidebar.text_input("OpenAI API key", type= "password")
 
-client = OpenAI(api_key = user_api_key)
-
-user_input = st.text_area("Enter some text to correct:", "your text heare")
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the tree key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
